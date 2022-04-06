d2629b47f7f16a1cba76c6ec48946fb5891649c1
noormohammad20 <mdsaim204@gmail.com>
2022-04-06 23:22:14 +0600

    Create new user using email and password on firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { getAuth } from "firebase/auth"
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
 import app from './firebase.init'
 import { Button, Form } from 'react-bootstrap'
+import { useState } from 'react'
 
 const auth = getAuth(app)
 
 function App() {
-  const handleEmailBlur = e => {
-    console.log(e.target.value)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmailBlur = event => {
+    setEmail(event.target.value)
   }
-  const handlePasswordBlur = e => {
-    console.log(e.target.value)
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value)
   }
-  const handleFormSubmit = e => {
-    e.preventDefault()
-    console.log('form submited')
+  const handleFormSubmit = event => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user
+        console.log(user)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    event.preventDefault()
   }
 
   return (
     <div>
       <div className="registration w-50 mx-auto">
         <h2 className='text-primary mt-5'>Please Register!!</h2>
-        <Form>
-          <Form.Group onBlur={handlePasswordBlur} className="mb-3" controlId="formBasicEmail">
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
@@ -33,10 +44,10 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
           </Form.Group>
 
-          <Button onSubmit={handleFormSubmit} variant="primary" type="submit">
+          <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
