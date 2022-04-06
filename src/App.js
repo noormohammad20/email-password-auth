74aa38f8be7da076ec49a1c71c26759b975b4077
noormohammad20 <mdsaim204@gmail.com>
2022-04-06 21:12:06 +0600

    added simple input field for email and passwore and submit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import app from './firebase.init'
 
 const auth = getAuth(app)
 
-
 function App() {
+  const handleEmailBlur = e => {
+    console.log(e.target.value)
+  }
+  const handlePasswordBlur = e => {
+    console.log(e.target.value)
+  }
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    console.log('form submited')
+  }
+
   return (
     <div className="App">
-      <form action="">
-        <input type="text" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleFormSubmit} action="">
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Login" />
       </form>
     </div>
   )
